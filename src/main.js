9590b544c9ef5639969b3f7cab0cb50526ab3c53
Julian Arana <julianarana@gmail.com>
2017-10-19 22:28:34 -0500

    Add React libraries and first components

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ExtrasApp extends React.Component{
+
+  render(){
+    return <div>
+      <h1>This is the very first element</h1>
+    </div>
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', () => ReactDOM.render(
+  React.createElement(ExtrasApp),
+  document.getElementById('app')
+))
